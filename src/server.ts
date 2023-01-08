2250bb8f424b8b2848428e20755132265cea2efd
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2023-01-08 21:04:38 +0545

    vercel deploy #1

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,7 @@ import http from "http";
 
 const PORT = process.env.PORT;
 ((): void => {
-  const server = http.createServer(application!);
-
+  // const server = http.createServer(application!);
   // server.listen(PORT, (): boolean =>
   //   process.stdout.write(
   //     `⚡️[Note-server]: Server is running at http://localhost:${PORT}\n`
